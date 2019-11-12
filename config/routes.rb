97d87ab1b93d6b18c6dbae5f3d256d0adb7af706
Tom Darby <tomd.57@live.com>
2019-11-12 17:13:33 +0100

    Task List

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
   patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
 
   # resources :tasks
 end
